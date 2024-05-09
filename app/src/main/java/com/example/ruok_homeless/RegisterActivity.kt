b0ef3c649965ceefea97ffaa7ca392f0113d2ce4
alimhanhan <alim11@naver.com>
2024-05-10 00:05:31 +0900

    add rendering

--- app/src/main/java/com/example/ruok_homeless/RegisterActivity.kt
+++ app/src/main/java/com/example/ruok_homeless/RegisterActivity.kt
@@ -1,12 +1,16 @@
 package com.example.ruok_homeless
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,12 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val signUpButton2 = findViewById<Button>(R.id.signUp_button2)
+        signUpButton2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java) // MainActivity로 이동하도록 수정
+            startActivity(intent)
+        }
+
     }
 }
